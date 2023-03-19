ad52e3ebe45a7b9e25c962b2e7bde164615d60eb
DESKTOP-5SC99UJ\philo <philomon@naver.com>
2023-03-20 01:11:55 +0900

    swagger path 변경

--- src/configs/swagger.ts
+++ src/configs/swagger.ts
@@ -3,12 +3,7 @@ import { DocumentBuilder } from '@nestjs/swagger';
 import { SwaggerModule } from '@nestjs/swagger/dist';
 
 export const setupSwagger = (app: INestApplication): void => {
-  const configSwagger = new DocumentBuilder()
-    .setTitle('테스트 API 명세서')
-    .setDescription('API 명세서 swagger 테스트')
-    .setVersion('0.0.1')
-    .addTag('?')
-    .build();
+  const configSwagger = new DocumentBuilder().setTitle('테스트 API 명세서').setDescription('API 명세서 swagger 테스트').setVersion('0.0.1').build();
   const document = SwaggerModule.createDocument(app, configSwagger);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/document', app, document);
 };
